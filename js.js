7cfcd90dacb7a41af0c0093484c5b63615af2e9d
Eren <vrsozluk@gmail.com>
2023-01-01 19:20:04 +0300

    I fixed the issue with the homepage and add function to the point of this project tab

--- js.js
+++ js.js
@@ -1,4 +1,6 @@
 const button = document.querySelector("#motivate");
+const point = document.querySelector('.point');
+
 button.addEventListener("click", myFunc);
 
 function myFunc() {
@@ -11,3 +13,12 @@ function myFunc() {
         (document.getElementById("quote").innerHTML = json[randomNumber])
     );
 }
+
+addEventListener('DOMContentLoaded', (event) => {
+ 
+  point.addEventListener('click', () => {
+    (document.getElementById("quote").innerHTML = `<div>This is one of my oldest projects.I made it to learn about JSON files, javascript, and bootstrap. </div>`)
+  })
+});
+
+
